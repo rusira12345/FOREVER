d296a6fdc02ca119f70e498b2c463466c786c932
rusira12345 <rusiradinujaya7@gmail.com>
2025-03-30 19:55:33 +0530

    I add the mobile responsive functionality to navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from "../assets/frontend_assets/assets"
 import { Link, NavLink } from 'react-router-dom'
 const Navbar = () => {
+    const [visible,setvisible] = useState(false);
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
         <img src={assets.logo} className='w-36'/>
@@ -39,7 +40,20 @@ const Navbar = () => {
                     <img src={assets.cart_icon} className='w-5 min-w-5' alt=''></img>
                     <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
             </Link>
-            <img src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden'/>
+            <img onClick={()=>setvisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden'/>
+        </div>
+        {/* Sidebar menu for small screens*/}
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+                <div className='flex flex-col text-gray-600'>
+                        <div onClick={()=>setvisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                                <img className='h-4 rotate-180' src={assets.dropdown_icon}/>
+                                <p>Back</p>
+                        </div>
+                        <NavLink onClick={()=>setvisible(false)} className="py-2 pl-6 border" to="/">HOME</NavLink>
+                        <NavLink onClick={()=>setvisible(false)} className="py-2 pl-6 border" to="/collections">COLLECTION</NavLink>
+                        <NavLink onClick={()=>setvisible(false)} className="py-2 pl-6 border" to="/about">ABOUT</NavLink>
+                        <NavLink onClick={()=>setvisible(false)} className="py-2 pl-6 border" to="/contact">CONTACT</NavLink>
+                </div>
         </div>
     </div>
   )
