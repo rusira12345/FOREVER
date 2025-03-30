4f97187900e2dbd0c2084054dc80c00958c75779
rusira12345 <rusiradinujaya7@gmail.com>
2025-03-31 04:31:05 +0530

    I develop the latest collection and best seller part in home page

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {products} from "../assets/frontend_assets/assets"
+import { createContext } from 'react'
+export const ShopContext  = createContext()
+const ShopContextProvider = (props) =>{
+    const currency = "$"
+    const delivery_fee = 10
+    const value = {
+            products,currency,delivery_fee
+    }
+    return(
+        <ShopContext.Provider value={value}>
+                {props.children}
+        </ShopContext.Provider>
+    )
+}
+export default ShopContextProvider
