6ef6e50868a36b314a051d45b3974f0027bb0875
rusira12345 <rusiradinujaya7@gmail.com>
2025-04-04 20:44:03 +0530

    i develop the cart page

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,14 +1,70 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {products} from "../assets/frontend_assets/assets"
 import { createContext } from 'react'
+import { toast } from 'react-toastify'
 export const ShopContext  = createContext()
 const ShopContextProvider = (props) =>{
-    const currency = "$"
+    const currency = "Rs "
     const delivery_fee = 10
     const [search,setSearch] = useState('');
     const [showSearch,setShowSearch] = useState(false);
+    const [cartItems,setCartItems] = useState({});
+    
+    
+    const addToCart = async(itemId,Size) =>{
+        {/*structured clone create a copy of the obejct sice cartItems is a object i use structured clone to
+            to get a copy of this object*/}
+            let cartData = structuredClone(cartItems);
+            if(!Size)
+            {
+                toast.error('Select Product Size');
+                return;
+            }
+            if(cartData[itemId])
+            {
+                if(cartData[itemId][Size])
+                {
+                    cartData[itemId][Size] += 1
+                }
+                else
+                {
+                    cartData[itemId][Size] = 1;
+                }
+            }
+            else
+            {
+                cartData[itemId] = {}
+                cartData[itemId][Size] = 1
+            }
+            setCartItems(cartData)
+    }
+    const getCardCount = () =>{
+        let totalCount = 0;
+        for(const items in cartItems)
+        {
+            for(const item in cartItems[items])
+            {
+                    try{
+                        if(cartItems[items][item]>0)
+                        {
+                            totalCount= totalCount + cartItems[items][item]
+                        }
+                    }catch(error)
+                    {
+
+                    }
+            }
+        }
+        return totalCount
+    }
+    const updateQuantity = async(itemId,Size,quantity) =>{
+            let cartData = structuredClone(cartItems);
+            cartData[itemId][Size] = quantity;
+            setCartItems(cartData)
+    }
     const value = {
-            products,currency,delivery_fee,search,setSearch,showSearch,setShowSearch
+        products,currency,delivery_fee,search,setSearch,showSearch,setShowSearch,
+        cartItems,addToCart,getCardCount,updateQuantity
     }
     return(
         <ShopContext.Provider value={value}>
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
-
+import React, { useContext, useEffect, useState } from 'react'
+import {ShopContext} from "../context/ShopContext"
+import Title from '../components/Title'
+import { assets } from '../assets/frontend_assets/assets'
 const Cart = () => {
+  const {cartItems,products,currency,updateQuantity} = useContext(ShopContext)
+  const [cartData,setcartData] = useState([])
+  useEffect(()=>{
+    const tempData = [];
+    for(const items in cartItems)
+    {
+      for(const item in cartItems[items])
+      {
+        if(cartItems[items][item]>0){
+          tempData.push({
+            _id:items,
+            size:item,
+            quantity:cartItems[items][item]
+          })
+        }
+      }
+    }
+    setcartData(tempData)
+  },[cartItems])
   return (
-    <div>
-      
+    <div className='border-t pt-14 border-gray-300'>
+      <div className='text-sxl mb-3'>
+          <Title text1={'YOUR'} text2={'CART'}/>
+      </div>
+      <div>
+        {
+          cartData.map((item,index)=>{
+            const productdata = products.find((product)=>product._id === item._id);
+            return(
+              <div key={index} className='py-4 border-t border-b border-gray-300 text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
+                  <div className='flex items-start gap-6'>
+                          <img className="w-16 sm:w-20" src={productdata.image[0]} alt="" />
+                          <div>
+                            <p className='text-xs sm:text-lg font-medium'>{productdata.name}</p>
+                            <div className='flex items-center gap-5 mt-2'>
+                                <p>{currency}{productdata.price}</p>
+                                <p className='px-2 sm:px-3 sm:py-1 border border-gray-300 bg-slate-50'>{item.size}</p>
+                            </div>
+                          </div>
+                  </div> 
+                   <input className="border border-gray-300 max-w-10 sm:max-w-20 px-1 sm:px-2 py-1 " type='number' onChange={(e)=>updateQuantity(item._id,item.size,Number(e.target.value))} min={1} defaultValue={item.quantity}/>
+                   <img onClick={()=>updateQuantity(item._id,item.size,0)} className="w-4 mr-4 sm:w-5 cursor-pointer" src={assets.bin_icon} alt=''></img>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
